ab0a11e62a08b135c133cb99de3a6d3267676408
QuanHoang17 <minhquandn1701@gmail.com>
2020-11-05 11:45:04 +0700

    form submit testing

---
+++ main.js
@@ -0,0 +1,12 @@
+
+let signUpForm = document.querySelector('.sign-up-form'); 
+signUpForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let teamName = document.getElementById('teamName').value;
+
+let email = document.getElementById('email').value;
+
+let numbMember = document.getElementById('numMember').value;
+console.log(numbMember);
+    
+})
