f78626111ba53b8da9600c7503a4354cf48a195d
dtnLong <49140143+dtnLong@users.noreply.github.com>
2020-11-08 09:46:48 +0700

    change from realtime database to firestore

--- public/main.js
+++ public/main.js
@@ -1,17 +1,41 @@
-const firebase_db = firebase.database()
+// const firebase_db = firebase.database()
+const firebase_db = firebase.firestore()
+
 let signUpForm = document.querySelector('.sign-up-form'); 
 signUpForm.addEventListener('submit', (e) => {
     e.preventDefault();
     let teamName = document.getElementById('teamName').value;
     let email = document.getElementById('email').value;
     let numbMember = document.getElementById('numMember').value;
+
     // Database Opearation
-    firebase_db.ref('members/' + teamName).set({
-        teamname: teamName,
+    let collection_ref = firebase_db.collection("members");
+    collection_ref.add({
+        teamName: teamName,
         email: email,
         numbMember: numbMember
+    }).then(function() {
+        console.log("Document successfully written!");
+    }).catch(function(error) {
+        console.error("Error writing document: ", error);
+    });;
+
+    firebase_db.collection("members").get().then(function(querySnapshot) {
+        // if (doc && doc.exists) {
+        //     const myData = doc.data();
+        //     console.log(myData);
+        // }
+        let dataList = []
+        querySnapshot.docs.map (doc => dataList.concat(doc.data()));
+    }).catch(function(error) {
+        console.log("Error: ", error);
     });
-    console.log(firebase_db.ref('members/').once('value').then((snapshot) => {
-        console.log(snapshot.val());
-    }));
+    // firebase_db.ref('members/' + teamName).set({
+    //     teamname: teamName,
+    //     email: email,
+    //     numbMember: numbMember
+    // });
+    // console.log(firebase_db.ref('members/').once('value').then((snapshot) => {
+    //     console.log(snapshot.val());
+    // }));
 })
